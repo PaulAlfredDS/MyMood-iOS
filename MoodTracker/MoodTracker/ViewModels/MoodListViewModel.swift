383837b02cb2cf19e52c5dbc490647ab34a253c8
Paul De Silva <paul.desilva.ns@gmail.com>
2025-05-26 22:53:44 +0800

    Update MoodListViewModel to conform to combine and fetching by month

--- MoodTracker/MoodTracker/ViewModels/MoodListViewModel.swift
+++ MoodTracker/MoodTracker/ViewModels/MoodListViewModel.swift
@@ -6,25 +6,54 @@
 //
 
 import Foundation
+import Combine
 
 
-class MoodListViewModel: ObservableObject {
-    
-    private var localDataSource: MoodDataSource
-    
-    @Published var moodEntries: [MoodEntry] = []
-    
-    
-    init(localDataSource: MoodDataSource) {
-        self.localDataSource = localDataSource
-    }
-    
-    func deleteMoodEntry(_ entry: MoodEntry) {
-        localDataSource.deleteMood(entry)
-        moodEntries = localDataSource.getMoodEntries()
-    }
-    
-    func fetchMoodEntries() {
-        moodEntries = localDataSource.getMoodEntries()
+extension MoodListView {
+    class MoodListViewModel: ObservableObject {
+        
+        private var localDataSource: MoodDataSource
+        
+        @Published var moods: [MoodEntry] = []
+        
+        private var cancellables = Set<AnyCancellable>()
+
+        
+        init(localDataSource: MoodDataSource) {
+            self.localDataSource = localDataSource
+        }
+        
+        func deleteMoodEntry(_ entry: MoodEntry) {
+            localDataSource.deleteMood(entry)
+            moods = localDataSource.getMoodEntries()
+        }
+        
+        func fetchMoodEntries(by month: Int) {
+            localDataSource.getMoodsbyMonth(month)
+                .publisher
+                .compactMap { mood in
+                    guard mood.moodEmoji != nil else {
+                        return nil
+                    }
+                    return mood
+                }
+                .collect()
+                .map { moods in
+                    moods.sorted { $0.date! > $1.date! }
+                }
+                .receive(on: RunLoop.main)
+                .sink { [weak self] sortedMoods in
+                    self?.moods = sortedMoods
+                }
+                .store(in: &cancellables)
+        }
+
+        
+        func formattedDay(from date: Date) -> String {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "d EEE" // e.g. 21 Tue
+            return formatter.string(from: date)
+        }
+
     }
 }
